cfde7c8c51c6fe0f29ebd4f71ad1f6c99fff8018
Prat <prat.s@iconext.co.th>
2018-03-16 08:37:31 +0700

    add TheCheeseLibrary

---
+++ app/src/main/java/com/example/prat/androidmvcstructure/MainApplication.java
@@ -0,0 +1,19 @@
+package com.example.prat.androidmvcstructure;
+
+import android.app.Application;
+
+/**
+ * Created by Prat on 3/15/2018.
+ */
+
+public class MainApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+
+    @Override
+    public void onTerminate() {
+        super.onTerminate();
+    }
+}
--- app/src/main/java/com/example/prat/androidmvcstructure/activity/MainActivity.java
+++ app/src/main/java/com/example/prat/androidmvcstructure/activity/MainActivity.java
@@ -1,7 +1,9 @@
-package com.example.prat.androidmvcstructure;
+package com.example.prat.androidmvcstructure.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.example.prat.androidmvcstructure.R;
 
 public class MainActivity extends AppCompatActivity {
 
