753f4cf3e03b4c06956f0ecaf33a9472a1e78826
Prat <prat.s@iconext.co.th>
2018-03-19 11:02:57 +0700

    add second fragment

--- app/src/main/java/com/example/prat/androidmvcstructure/activity/MainActivity.java
+++ app/src/main/java/com/example/prat/androidmvcstructure/activity/MainActivity.java
@@ -1,14 +1,25 @@
 package com.example.prat.androidmvcstructure.activity;
 
+import android.app.Fragment;
+import android.content.Context;
 import android.os.Build;
+import android.os.PersistableBundle;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.AttributeSet;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
 import android.widget.Toast;
 
 import com.example.prat.androidmvcstructure.R;
 import com.example.prat.androidmvcstructure.fragment.MainFragment;
+import com.example.prat.androidmvcstructure.fragment.SecondFragment;
 import com.example.prat.androidmvcstructure.util.ScreesUtils;
 import com.inthecheesefactory.thecheeselibrary.manager.Contextor;
+
+import java.util.zip.Inflater;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -37,5 +48,45 @@ public class MainActivity extends AppCompatActivity {
         } else {
 
         }
+    }
+
+    @Override
+    protected void onPostCreate(@Nullable Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+
+        if(savedInstanceState == null)
+        {
+            MainFragment mainFragment = (MainFragment) getSupportFragmentManager().findFragmentById(R.id.contentContainer);
+            mainFragment.setHelloText("Woo Hoooooooo");
+        }
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        if(item.getItemId() == R.id.toSecondFragment)
+        {
+            android.support.v4.app.Fragment fragment = getSupportFragmentManager().findFragmentById(R.id.contentContainer);
+            if(fragment instanceof  SecondFragment == false)
+            {
+                Toast.makeText(MainActivity.this, "Second Fragment", Toast.LENGTH_SHORT).show();
+                //some thing after click this menu
+                getSupportFragmentManager().beginTransaction()
+                        .add(R.id.contentContainer, SecondFragment.newInstance())
+                        .commit();
+            }
+
+
+        }
+        return true;
     }
 }
--- app/src/main/java/com/example/prat/androidmvcstructure/fragment/MainFragment.java
+++ app/src/main/java/com/example/prat/androidmvcstructure/fragment/MainFragment.java
@@ -6,6 +6,7 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.example.prat.androidmvcstructure.R;
 import com.example.prat.androidmvcstructure.activity.MainActivity;
@@ -15,11 +16,43 @@ import com.example.prat.androidmvcstructure.activity.MainActivity;
  */
 
 public class MainFragment extends Fragment {
+
+    TextView textviewHello;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+        initInstances(rootView);
+
 
         return rootView;
+    }
+
+    private void initInstances(View view)
+    {
+        textviewHello = view.findViewById(R.id.textviewHello);
+    }
+
+    public void setHelloText(String text)
+    {
+        textviewHello.setText(text);
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+
+
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        if(savedInstanceState != null)
+        {
+
+        }
     }
 }
